c9f9beae2963e6d1f168afc125061c4844b32f5d
nitro2 <ngothanhnhannt@gmail.com>
2017-09-04 23:00:31 +0700

    upload utilities

---
+++ utilities.py
@@ -0,0 +1,14 @@
+import inspect
+
+class ERROR:
+	CMD_SUCCESS              =  0   #Command is success
+	CONNECTION_FAIL          = -1	#Bittrex api JSON cannot connect to server
+	CMD_UNSUCCESS            = -2	#Bittrex api JSON return "success"=False
+	CMD_INVALID              = -3	#Bittrex api not supported. Ex: wrong market name.
+	TIME_OUT                 = -4	#A cmd cannot be completed within a specific time.
+	PARAMETERS_INVALID       = -5	#Inputs for api are invalid. Ex: "INSUFFICIENT_FUNDS"
+
+def err_line_track():
+    """Returns the current line number in our program."""
+    return inspect.currentframe().f_back.f_lineno
+	
