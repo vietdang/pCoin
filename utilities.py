631e5259bba63a07a21db04f567a72588c8dd5da
nitro2 <ngothanhnhannt@gmail.com>
2017-09-13 23:52:17 +0700

    Add math api

--- utilities.py
+++ utilities.py
@@ -12,4 +12,34 @@ class ERROR:
 def err_line_track():
 	"""Returns the current line number in our program."""
 	return inspect.currentframe().f_back.f_lineno
+
+def mean(data):
+    """Return the sample arithmetic mean of data."""
+    n = len(data)
+    if n < 1:
+        return 0
+    return sum(data)/n # in Python 2 use sum(data)/float(n)
+
+def ssdev(data):
+    """Return sum of square deviations of sequence data."""
+    c = mean(data)
+    ss = sum((x-c)**2 for x in data)
+    return ss
 	
+def pstdev(data):
+    """Calculates the population standard deviation."""
+    n = len(data)
+    if n < 2:
+        return 0
+    ss = ssdev(data)
+    pvar = ss/n # the population variance
+    return pvar**0.5
+
+def stddev(data):
+    """Calculates the sample standard deviation."""
+    n = len(data)
+    if n < 2:
+        return 0
+    ss = ssdev(data)
+    pvar = ss/(n-1) # the sample variance
+    return pvar**0.5
